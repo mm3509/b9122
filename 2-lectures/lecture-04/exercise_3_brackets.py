a3041a509a9dbd0495e58af3ae4004ca6947ce95
Miguel <32396311+miguelmorin@users.noreply.github.com>
2025-09-20 00:23:03 -0400

    added lecture 4

---
+++ 2-lectures/lecture-04/exercise_3_brackets.py
@@ -0,0 +1,25 @@
+def is_valid(s):
+    """
+    >>> is_valid("{}[]{}()")
+    True
+    >>> is_valid("{}[}")
+    False
+    >>> is_valid("{{{[()]}}}")
+    True
+    >>> is_valid("}")
+    False
+    >>> is_valid("{{}")
+    False
+    >>> is_valid("{a}")
+    False
+    >>> is_valid("")
+    True
+    >>> is_valid(123)
+    Traceback (most recent call last):
+    ...
+    ValueError: argument must be a string
+    """
+
+    # TODO: implement this function.
+
+    return
