256f4b03fb5f201e45a9d195848b9cc5f721051f
Miguel <32396311+miguelmorin@users.noreply.github.com>
2025-09-24 11:04:14 -0400

    removed fizzbuzz

--- 2-lectures/lecture-04/exercise_2_fizzbuzz.py
+++ 2-lectures/lecture-04/exercise_2_fizzbuzz.py
@@ -4,11 +4,3 @@ for i in range(100):
     print(i)
 
     # TODO: complete this inner loop
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
