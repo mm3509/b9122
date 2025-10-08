d7d5bc99cb5e4f28dd181acd06cd6ef547d1ef6c
Miguel <32396311+miguelmorin@users.noreply.github.com>
2025-10-08 14:05:40 -0400

    added lecture 6, afternoon

---
+++ 2-lectures/lecture-06-afternoon/exercise_10_dictionaries.py
@@ -0,0 +1,28 @@
+import statistics as st
+from statistics import mean
+
+portfolio = {'MSFT': 30, 'AAPL': 30, 'GOOG': 20, 'NVDA': 20}
+
+for ticker in portfolio:
+    print('Ticker', ticker, 'has', portfolio[ticker], 'shares')
+
+print('Total number of shares:', sum(portfolio.values()))
+
+portfolio['GTLB'] = portfolio.get('GTLB', 0) + 10
+portfolio['MSFT'] = portfolio.get('MSFT', 0) - 10
+
+print('Total number of shares:', sum(portfolio.values())
+
+# Computation with an alias.
+shares = portfolio.values()
+avg = st.mean(shares)
+print('Average number of shares:', avg)
+
+print('  Mean:', st.mean(shares))
+print('  Mode:', st.mode(shares))
+print('Median:', st.median(shares))
+print('St-dev:', st.stdev(shares))
+
+
+# Computation with a `from ... import ...`.
+print('  Mean:', mean(shares))
---
+++ 2-lectures/lecture-06-afternoon/tools.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+
+
+class YahooError(Exception):
+    pass
+
+
+def get_yahoo_value(code):
+    """
+    >>> a = get_yahoo_value("EURUSD=X")
+    >>> a > 1.1
+    True
+    >>> a < 1.2
+    True
+    >>> get_yahoo_value(123)
+    Traceback (most recent call last):
+    ...
+    TypeError: argument must be ...
+    """
+
+    if not isinstance(code, str):
+        raise TypeError("argument must be a string")
+
+    dataframe = yf.download(code, progress=False, auto_adjust=True)
+    closing_value_series = dataframe[["Close"]]
+
+    num_obs = closing_value_series.shape[0]
+    if num_obs == 0:
+        raise YahooError("Yahoo did not return any data")
+
+    latest_date = max(closing_value_series.index)
+
+    latest_value_as_series = closing_value_series.loc[latest_date]
+    latest_value_as_float = float(latest_value_as_series.iloc[0])
+
+    return latest_value_as_float
+
+
+def get_yahoo_price(code):
+    return get_yahoo_value(code)
