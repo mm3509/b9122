15619138eedeb2609fdffb2254a4b7470819b351
Miguel <32396311+miguelmorin@users.noreply.github.com>
2025-10-22 13:45:55 -0400

    added lecture 7 afternoon

---
+++ 2-lectures/lecture-07-afternoon/exercise_15_factorial_recursive.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    """
+    >>> factorial(3)
+    6
+    >>> factorial(4)
+    24
+    """
+
+    # TODO: add defensive programming.
+
+    # TODO: implement the function to pass the doc-tests.
+
+    return
