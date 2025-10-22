eb64f240bb9a5b199f81c23e6ff90026da5837f3
Miguel <32396311+miguelmorin@users.noreply.github.com>
2025-10-22 15:16:56 -0400

    added exercise 21

---
+++ 2-lectures/lecture-07-afternoon/exercise_21_uniqueness.py
@@ -0,0 +1,19 @@
+def get_unique(alist):
+    """
+    >>> get_unique([2, 2, 4, 4, 6, 8, 9, 10, 10])
+    [2, 4, 6, 8, 9, 10]
+    >>> get_unique([2, 4, 6, 8, -9, 10])
+    [-9, 2, 4, 6, 8, 10]
+    >>> get_unique(123)
+    Traceback (most recent call last):
+    ...
+    AssertionError
+    >>> get_unique([2.5, 6])
+    Traceback (most recent call last):
+    ...
+    AssertionError
+    """
+
+    # TODO: add defensive programming.
+
+    # TODO: implement the function.
